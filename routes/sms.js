716774461e681c4127c2e816bd0970e53e6b8a23
dylansqing <dylansqing@outlook.com>
2020-12-14 14:22:14 +0800

    Update sms.js

--- routes/sms.js
+++ routes/sms.js
@@ -31,8 +31,8 @@ var setOptionMaintain = {
 }
 //-------------------------------------------------------------------------------------------------------------------------
 const SMSClient = require('@alicloud/sms-sdk')
-const accessKeyId = 'LTAIBbEYy05fK8Zb'
-const secretAccessKey = 'rQ7HpvyFhpnDZKlBN4djQj90EjukZt'
+const accessKeyId = 'LTAIB=>bEYy05fK8Zb'
+const secretAccessKey = 'rQ7Hpvy=>FhpnDZKlBN4djQj90EjukZt'
 
 const smsClient = new SMSClient({
     accessKeyId,
